4998ad07e74bda3ef0e70d661f78174877657767
sachinlokesh05 <sachin.beee.15@acharya.ac.in>
2020-07-02 21:34:55 +0530

    rendering Lists

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,13 +3,19 @@ import { Button } from 'react-bootstrap';
 
 
 class Counter extends Component {
-    state = { count : 3}
+    state = { count : 3,
+    
+        tags : ['tag1','tag2','tag3']
+    
+    }
 
     render() { 
         
         return (<React.Fragment><span style={ {fontSize:20 }} className={this.getBadgeClasses() } >{this.formatCount()}</span><Button variant="dark m-2 " size="md">
         Small button
-        </Button></React.Fragment>);
+        </Button>
+        <ul>{this.state.tags.map(tag => <li key={tag} >{tag}</li> )}</ul>
+        </React.Fragment>);
     }
     getBadgeClasses() {
         let classes = "badge m-3 badge-";
