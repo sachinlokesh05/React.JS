1a8a6bdae724d76a143b1755cae46d0447eb8668
sachinlokesh05 <sachin.beee.15@acharya.ac.in>
2020-07-02 21:07:56 +0530

    added bootstrap

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
+import { Button } from 'react-bootstrap';
 
 
 class Counter extends Component {
     state = { count : 0}
     render() { 
-    return (<React.Fragment><span>{this.formatCount()}</span><button>Increment</button></React.Fragment>);
+    return (<React.Fragment><span  className="badge badge-primary m-3" >{this.formatCount()}</span><Button variant="dark" size="md">
+    Small button
+  </Button></React.Fragment>);
     }
     formatCount (){
         const {count} = this.state;
-        return count === 0 ? <h1>Zero</h1> : count;
+        return count === 0 ? <h5>Zero</h5> : count;
     };
 }
  
