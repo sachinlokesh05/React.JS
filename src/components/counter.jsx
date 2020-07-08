e175bbaebff336fb892f0e453a34764b4616d49d
sachinlokesh05 <sachin.beee.15@acharya.ac.in>
2020-07-08 22:02:50 +0530

    Encapsulating props

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 // import {Alert } from 'react-bootstrap';
 
 class Counter extends Component {
-    state = { count : this.props.value
+    state = { count : this.props.counter.value
         };
     // constructor(){
     //     super();
@@ -31,10 +31,10 @@ class Counter extends Component {
 
     render() {                 
         return (<React.Fragment>
-            <h1>{this.props.id}</h1>
+            <h1>{this.props.counter.id}</h1>
             <span style={ {fontSize:20 }} className={this.getBadgeClasses() } >{this.formatCount()}</span>
             <button className="btn btn-dark btn-lg m-2" onClick={this.handleIncrement} >Increment</button>
-            <button className="btn btn-danger btn-lg m-2" onClick={()=>this.props.onDelete(this.props.id)} >Delete</button>
+            <button className="btn btn-danger btn-lg m-2" onClick={()=>this.props.onDelete(this.props.counter.id)} >Delete</button>
             {/* {this.state.tags.length === 0  && <Alert variant="info m-2  " > <h3>Please Create a new tags!!</h3> </Alert>} 
             <Alert variant="danger m-2  " > {this.renderTags()} </Alert>  */}
             
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -17,7 +17,7 @@ class Counters extends Component {
         return ( 
             <div>
             {this.state.counter.map(counter => 
-            <Counter key={counter.id} onDelete={this.handleDelete} value={counter.value} id={counter.id} >
+            <Counter key={counter.id} onDelete={this.handleDelete} counter={counter} >
             </Counter>)}
             </div>
         );
