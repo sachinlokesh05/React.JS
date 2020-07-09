6ed33f605184d98eb1916eb42b85c93bdb883077
sachinlokesh05 <sachin.beee.15@acharya.ac.in>
2020-07-09 20:39:04 +0530

    Multiple components in Sync

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import Counter from './components/counter'
-import Counters from "./components/counters"
 ReactDOM.render(
-  <React.StrictMode>
-    <Counters/>
-   </React.StrictMode>,
+ <App/>,
   document.getElementById('root')
 );
 
