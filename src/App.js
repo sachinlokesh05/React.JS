edc3152b48fb181f4417569dccaa22910c5ec72e
sachinlokesh05 <sachin.beee.15@acharya.ac.in>
2020-07-10 21:32:17 +0530

    lifting the state Up

--- src/App.js
+++ src/App.js
@@ -1,17 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/navBar'
 import Counters from "./components/counters"
 
-function App() {
+class App extends Component {
+
+  state = {  counters:[
+    { id:1,value:0},
+    { id:2,value:0},
+    { id:3,value:0},
+    { id:4,value:0}
+    ]  };
+
+    handleDelete = counterId => {
+        const counters = this.state.counters.filter(c => c.id !== counterId );
+        this.setState({counters});
+        
+    };
+
+    handleIncrement = counter => {
+        const counters = [...this.state.counters];
+        const index = counters.indexOf(counter);
+        counters[index] = {...counter};
+        counters[index].value++;        
+        this.setState({counters});
+
+    };
+
+    handleReset = () => {
+        const counters = this.state.counters.map(c => {
+            c.value = 0;
+            return c ;
+        });
+        this.setState({counters});
+    }
+
+render(){
   return (
     <React.Fragment>
-    <Navbar/>
+    <Navbar totalCounters={this.state.counters.filter(c => c.value >0).length}/>
     <main className="container">
-      <Counters/>
+      <Counters
+      onIncrement={this.handleIncrement} 
+      onDelete={this.handleDelete} 
+      onReset={this.handleReset}
+      counters={this.state.counters} 
+      />
     </main>
     </React.Fragment> 
   );
 }
-
+}
 export default App;
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -4,10 +4,16 @@ class Navbar extends Component {
     render() { 
         return ( 
             <nav className="navbar   navbar-light bg-light">
-                <a className="navbar-brand" href="/#">Navbar</a>
+                <a className="navbar-brand" href="/#"> 
+                NavBar{" "}
+                    <span className="badge badge-pill badge-secondary">
+                        {this.props.totalCounters}
+                    </span>
+                </a>
             </nav>
          );
     }
 }
+
  
 export default Navbar;
